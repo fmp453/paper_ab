425038d5787a8c8cdf76c9da388c1a1f2c55696e
fmuuly <fuchitadashion@masane-MBP.local>
2023-06-13 11:31:46 +0900

    tag table作成コードの追加

--- database/make_csv.py
+++ database/make_csv.py
@@ -1,6 +1,13 @@
+import os
 import pandas as pd
 
 
 df = pd.DataFrame(columns=['id', 'title', 'abstract'])
 
-df.to_csv('paper_info.csv', index=False)
+if not os.path.exists('paper_info.csv'):
+    df.to_csv('paper_info.csv', index=False)
+
+df_tag = pd.DataFrame(columns=['tag_name', 'tag_id'])
+
+if not os.path.exists('tags_table.csv'):
+    df_tag.to_csv('tags_table.csv', index=False)
