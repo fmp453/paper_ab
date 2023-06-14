381222dbfcbeb2a4670c7381e2b3806d9da6a5e7
fmuuly <64724985+fmp453@users.noreply.github.com>
2023-06-14 14:08:53 +0900

    tagを管理するクラス

---
+++ lib/utils/utils.dart
@@ -0,0 +1,19 @@
+
+class PaperTags {
+  final tags = [
+    'Image',
+    'NLP',
+    'Audio',
+    'Video',
+    '時系列',
+    '理論',
+    'GAN',
+    '拡散モデル',
+    '強化学習',
+    'グラフ',
+  ];
+
+  List<String> getTagList(){
+    return tags;
+  }
+}
